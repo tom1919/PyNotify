de466e26e489854d83e0a05ffbbb36b9f63ab01c
Tommy Lang <35646257+tom1919@users.noreply.github.com>
2021-10-20 23:54:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: Apache License 2.0',   
+    'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3',     
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
