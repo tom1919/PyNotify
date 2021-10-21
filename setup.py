002d000e7574e9e995b3e218b13ef7d5c464c433
Tommy Lang <35646257+tom1919@users.noreply.github.com>
2021-10-20 23:41:49 -0400

    update install_requires

--- setup.py
+++ setup.py
@@ -10,9 +10,12 @@ setup(
   url = 'https://github.com/tom1919',  
   download_url = 'https://github.com/tom1919/PyNotify/archive/refs/tags/v0.0.1.tar.gz',   
   keywords = ['email', 'logging', 'timimg', 'notification'],  
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  install_requires=[
+          'email',
+          'pandas',
+          'datetime',
+          'tabulate',
+          'functools',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
